d4c8e96b1163680ba58527833ad5517d7be9e7c1
Melissa Illig <melissaillig@Melissas-MacBook-Pro.local>
2020-11-16 11:18:09 +0100

    upload all Assets

---
+++ Assets/MyGame/Scripts/GameScript.cs
@@ -0,0 +1,75 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameScript : MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+    public Text textGuess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        max = 100;
+        min = 1;
+        guess = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    /*    if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            guessNext();
+            Debug.Log("Higher");
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            guessNext();
+            Debug.Log("Lower");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("correct Guess");
+            SceneManager.LoadScene("FinishedScene", LoadSceneMode.Single);
+        }
+*/
+
+    }
+
+    void guessNext()
+    {
+        guess = (min + max) / 2;
+        textGuess.text = guess.ToString();
+    }
+
+    public void ButtonLow()
+    {
+        max = guess;
+        guessNext();
+        Debug.Log("Lower");
+
+    }
+
+    public void ButtonHigh()
+    {
+        min = guess;
+        guessNext();
+        Debug.Log("Higher");
+
+    }
+
+    public void ButtonCorrect()
+    {
+        Debug.Log("correct guess");
+        SceneManager.LoadScene("FinishedScene", LoadSceneMode.Single);
+
+    }
+}
